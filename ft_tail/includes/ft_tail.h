8d193bbb1852c38917cc73b6fd508493291e02f2
澤田慶 <kei@sawadakeinoMacBook-Pro.local>
2023-09-13 11:58:53 +0900

    completed n option!

--- ft_tail/includes/ft_tail.h
+++ ft_tail/includes/ft_tail.h
@@ -6,7 +6,7 @@
 /*   By: kei <kei@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 21:37:41 by kei               #+#    #+#             */
-/*   Updated: 2023/09/13 10:17:10 by kei              ###   ########.fr       */
+/*   Updated: 2023/09/13 11:30:55 by kei              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@
 
 #define BUFFER_SIZE 512
 
-typedef struct {
-    int lines;
+typedef struct s_options
+{
+    int n;
     char **filenames;
     int file_count;
 	int plus_flag;
-} TailOptions;
+	int n_flag;
+} t_options;
 
 int		ft_strcmp(const char *str1, const char *str2);
 int		ft_strncmp(const char *str1, const char *str2, size_t num);
@@ -40,21 +42,24 @@ void	*ft_realloc(void *ptr, size_t original_size, size_t new_size);
 char	**ft_split(char *str, char *delimiter);
 size_t	ft_count_elements(char **array);
 
-void	ft_tail_no_option(char *filename);
+void	display_no_option(char *filename);
+void	display_n_option(t_options *options, int index);
 
 void	ft_tail(int argc, char **argv);
-void	display_tail_error(char *filename);
+void	display_tail_error(const char *filename);
 char	*get_next_line(char **start_pos);
 void	display_filename(const char *filename);
 void	handle_lines(char *start_pos, char **lines, int *current_line);
 void	display_lines(char **buffer_split, int first, int last);
-char	*dynamic_read(int fd, size_t *total_size);
-
-void	parse_options(int argc, char **argv, TailOptions *options);
-void 	execute_tail(TailOptions *options);
+void	parse_options(int argc, char **argv, t_options *options);
+void 	execute_tail(t_options *options);
 
 void	free_memory(void **any, size_t length);
 void	**alloc_memory(size_t num_elements);
 
+int		open_file(const char *filename);
+char	*dynamic_read(int fd);
+char	*read_file_content(int fd);
+char	*expand_buffer(char *buffer, size_t total_read, size_t *current_block_size);
 
 #endif
