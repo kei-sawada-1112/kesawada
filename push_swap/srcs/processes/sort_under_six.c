81d881931ab1535299e5ed1cf10d1d399364f9bc
澤田慶 <kei@sawadakeinoMacBook-Pro.local>
2023-10-25 11:12:27 +0900

    quick_sort_b.c => sort_under_six.c

--- push_swap/srcs/processes/sort_under_six.c
+++ push_swap/srcs/processes/sort_under_six.c
@@ -6,7 +6,7 @@
 /*   By: kesawada <kesawada@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:07:23 by kesawada          #+#    #+#             */
-/*   Updated: 2023/10/25 10:19:58 by kesawada         ###   ########.fr       */
+/*   Updated: 2023/10/25 10:37:31 by kesawada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,47 +58,41 @@ int	check_valid_operation(t_ms *ms, int operation)
 	return (1);
 }
 
-void	sort_under_six(t_stack **a, t_stack **b, t_ms *ms, int count)
+int	execute(t_stack **a, t_stack **b, t_ms *ms, int op)
 {
-	int			op;
-
 	static t_operation f[] =
 	{
 		swap_a, swap_b, swap_ab, push_a, push_b,\
 		rotate_a, rotate_b, rotate_ab,\
 		rotate_rev_a, rotate_rev_b, rotate_rev_ab
 	};
-	static t_operation f_rev[] =
+
+	return (f[op](a, b, ms));
+}
+
+int	execute_rev(t_stack **a, t_stack **b, t_ms *ms, int op)
+{
+	static t_operation f[] =
 	{
 		swap_a, swap_b, swap_ab, push_b, push_a,\
 		rotate_rev_a, rotate_rev_b, rotate_rev_ab,\
 		rotate_a, rotate_b, rotate_ab
 	};
+
+	return (f[op](a, b, ms));
+}
+
+void	sort_under_six(t_stack **a, t_stack **b, t_ms *ms, int count)
+{
+	int			op;
+
 	if (count > ms->limit_count)
 		return ;
-	if (((*b)->next->is_separator) && in_order(*a))
+	if (((*b)->next->is_separator) && in_order(*a) && ms->count > count)
 	{
-		if (ms->count > count)
-		{
-			(*a) = (*a)->next;
-			while ((*a)->next && !(*a)->is_separator)
-			{
-				// ft_printf("index: %d\n", a->index);
-				ft_printf("value: %d\n", (*a)->value);
-				// ft_printf("value: %d\n", a->next->value);
-				// ft_printf("value: %d\n", a->next->next->value);
-				// ft_printf("*-----------*\n");
-				(*a) = (*a)->next;
-			}
-			ms->count = count;
-			// while (ms->op_list)
-			// {
-			// 	ft_printf("------op_list: %d\n------\n", ms->op_list->op);
-			// 	ms->op_list = ms->op_list->next;
-			// }
-			clear_operation(&ms->actual_op);
-			copy_operation(ms, ms->op_list);
-		}
+		ms->count = count;
+		clear_operation(&ms->actual_op);
+		copy_operation(ms, ms->op_list);
 		return ;
 	}
 	op = -1;
@@ -106,28 +100,12 @@ void	sort_under_six(t_stack **a, t_stack **b, t_ms *ms, int count)
 	{
 		if (!check_valid_operation(ms, op) || ms->count <= count)
 			continue ;
-		if (!f[op](a, b, ms))
+		if (!execute(a, b, ms, op))
 			continue ;
 		ms->op = op;
 		add_operation(&ms->op_list, op);
 		sort_under_six(a, b, ms, count + 1);
-		f_rev[op](a, b, ms);
+		execute_rev(a, b, ms, op);
 		delone_operation(&ms->op_list);
 	}
 }
-
-// int	quick_sort_b(t_stack **a, t_stack **b, t_ms *ms, int count)
-// {
-// 	int	operation;
-
-// 	static t_operation f[] =
-// 	{
-// 		swap_a, swap_b, swap_ab, push_a, push_b,
-// 		rotate_a, rotate_b, rotate_rev_a, rotate_rev_b,
-// 		rotate_rev_ab
-// 	};
-// 	if (in_order(*b))
-// 		return (count);
-
-
-// }
