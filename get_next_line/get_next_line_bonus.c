da58110d79cdf01a62630b7c3c7739476bea80e4
澤田慶 <kei@sawadakeinoMacBook-Pro.local>
2023-11-10 14:17:27 +0900

    commit忘れ

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -6,22 +6,26 @@
 /*   By: kesawada <kesawada@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:36:00 by kesawada          #+#    #+#             */
-/*   Updated: 2023/11/02 14:20:00 by kesawada         ###   ########.fr       */
+/*   Updated: 2023/11/10 14:10:25 by kesawada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static void	init_param(t_ms *new, int fd)
+static void	free_all(t_ms *dummy, t_ms *ms)
 {
-	new->fd = fd;
-	new->state = LETTER;
-	new->tmp_buffer = NULL;
-	new->cap = BUFFER_SIZE;
-	new->tmp_len = 0;
-	new->copied_len = 0;
-	new->count = 0;
-	new->next = NULL;
+	t_ms	*prev;
+
+	free(ms->tmp_buffer);
+	ms->tmp_buffer = NULL;
+	free(ms->buffer);
+	ms->buffer = NULL;
+	prev = dummy;
+	while (prev->next && prev->next != ms)
+		prev = prev->next;
+	if (prev->next)
+		prev->next = ms->next;
+	free(ms);
 }
 
 static t_ms	*init_ms(int fd)
@@ -31,14 +35,16 @@ static t_ms	*init_ms(int fd)
 	new = malloc(sizeof(t_ms));
 	if (!new)
 		return (NULL);
+	*new = (t_ms){0};
+	new->fd = fd;
 	new->cap = BUFFER_SIZE;
-	new->buffer = malloc(new->cap + (size_t)1);
+	new->buffer = malloc(BUFFER_SIZE + 1);
 	if (!new->buffer)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->bytes_read = read(fd, new->buffer, new->cap);
+	new->bytes_read = read(fd, new->buffer, BUFFER_SIZE);
 	if (new->bytes_read < 0)
 	{
 		free(new->buffer);
@@ -46,37 +52,7 @@ static t_ms	*init_ms(int fd)
 		return (NULL);
 	}
 	new->buffer[new->bytes_read] = '\0';
-	init_param(new, fd);
 	return (new);
-}
-
-static int	endline_function(t_ms *dummy, t_ms *ms, char **next_line)
-{
-	t_ms	*prev;
-
-	if (!ms->count && !ms->tmp_len)
-	{
-		if (ms->tmp_buffer)
-		{
-			free(ms->tmp_buffer);
-			ms->tmp_buffer = NULL;
-		}
-		if (ms->buffer)
-		{
-			free(ms->buffer);
-			ms->buffer = NULL;
-		}
-		prev = dummy;
-		while (prev->next && prev->next != ms)
-			prev = prev->next;
-		if (!prev->next)
-			return (0);
-		prev->next = ms->next;
-		free(ms);
-		return (0);
-	}
-	set_next_line(ms, next_line);
-	return (1);
 }
 
 static t_ms	*get_current_ms(t_ms *ms, int fd)
@@ -97,14 +73,38 @@ static t_ms	*get_current_ms(t_ms *ms, int fd)
 	return (current);
 }
 
+int	state_based_process(t_ms *dummy, t_ms *ms, char **next_line)
+{
+	if (ms->state == LETTER)
+		read_letter(ms);
+	else if (ms->state == NEED_READ)
+		re_read(ms);
+	else if (ms->state == NEWLINE || ms->state == GNL_EOF)
+	{
+		if (!ms->count && !ms->tmp_len)
+		{
+			free_all(dummy, ms);
+			return (-1);
+		}
+		set_next_line(ms, next_line);
+		return (1);
+	}
+	else if (ms->state == ERROR)
+	{
+		free_all(dummy, ms);
+		return (-1);
+	}
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
 	static t_ms	dummy;
 	char		*next_line;
 	t_ms		*c_ms;
+	int			output;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX \
-		|| read(fd, &next_line, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	dummy.fd = INT_MIN;
 	c_ms = get_current_ms(&dummy, fd);
@@ -112,16 +112,11 @@ char	*get_next_line(int fd)
 		return (NULL);
 	while (1)
 	{
-		if (c_ms->state == LETTER)
-			read_letter(c_ms);
-		else if (c_ms->state == NEED_READ)
-			re_read(c_ms);
-		else if (c_ms->state == NEWLINE || c_ms->state == GNL_EOF)
-		{
-			if (!endline_function(&dummy, c_ms, &next_line))
-				return (NULL);
-			break ;
-		}
+		output = state_based_process(&dummy, c_ms, &next_line);
+		if (output == 1)
+			return (next_line);
+		if (output == -1)
+			return (NULL);
 	}
 	return (next_line);
 }
